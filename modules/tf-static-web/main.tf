fd476e9ca3d3679e3c1568e9cbeb0b9622591757
raz-project <Srazi88@walla.co.il>
2025-08-04 07:38:53 +0300

    Update static main

--- modules/tf-static-web/main.tf
+++ modules/tf-static-web/main.tf
@@ -49,6 +49,8 @@ resource "aws_s3_bucket_policy" "public_read" {
       }
     ]
   })
+
+  depends_on = [aws_s3_bucket_public_access_block.my_bucket_public_access_block]
 }
 
 ####################################################
